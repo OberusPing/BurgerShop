59ea6f4c4d0f3236be4ca0799947899d1913d93e
Achyuth <achyuthc@gmail.com>
2022-06-13 14:17:29 -0400

    Added condiments to JSON and updated order function.

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ with open('menu.json', 'r') as f:
 
 for category in data['food-item-categories']:
     if category['name'] == 'Burgers':
-        burgers = [category['menu-items']]
+        burgers = category['menu-items']
     if category['name'] == 'Sides':
-        sides = [category['menu-items']]
+        sides = category['menu-items']
     if category['name'] == 'Drinks':
-        drinks = [category['menu-items']]
+        drinks = category['menu-items']
+    if category['name'] == 'Condiments':
+        condiments = category['menu-items']
 
 # implement the classes listed below
 
@@ -50,8 +52,15 @@ class Side(FoodItem):
     pass
 
 
-class Combo(FoodItem):
-    pass
+class Combo(Burger, Drink, Side):
+    burger = None
+    side = None
+    drink = None
+
+    def __init__(self, burger: Burger, drink: Drink, side: Side) -> None:
+        self.burger = burger
+        self.drink = drink
+        self.side = side
 
 
 class Order:
@@ -66,34 +75,43 @@ class Order:
         self.food_items.append(FoodItem)
         self.order_price += FoodItem.price
 
-    def remove_item(self, FoodItem: FoodItem) -> None:
-        self.foodItems.remove(FoodItem)
-        self.order_price -= FoodItem.price
+    def remove_item(self) -> None:
+        item, index = pick(
+            self.food_items, "Please select the item to remove.")
+        self.foodItems.remove(item)
+        self.order_price -= item.price
 
     def calculate_price(self) -> float:
         return self.order_price
 
-    def return_order(self) -> str:
-        pass
+    def review_order(self) -> None:
+        print("Your current order is the following: \n")
+        for item in self.food_items:
+            print(f"{item.name}: {item.price} ({item.description})\n")
+        print(f"Current total price: {self.order_price}")
 
 
 def user_input_burger() -> Burger:
     burger_names = []
-    for burger in burgers[0]:
+    for burger in burgers:
         burger_names.append(burger['name'])
+
+    condiment_names = []
+    for condiment in condiments:
+        condiment_names.append(condiment['name'])
 
     # ask user for input and store it in burger object
     option, index = pick(
         burger_names, "Please select a burger to add to your order.")
 
-    b_name = burgers[0][index]['name']
-    b_price = burgers[0][index]['price']
-    b_description = burgers[0][index]['description']
+    b_name = burgers[index]['name']
+    b_price = burgers[index]['price']
+    b_description = burgers[index]['description']
 
     # ask user if they want to add condiments
     b_condiments = []
 
-    options = pick(["ketchup", "mustard"], "Please select any condiments to add to your burger (press SPACE to mark, ENTER to continue).",
+    options = pick(condiment_names, "Please select any condiments to add to your burger (press SPACE to mark, ENTER to continue).",
                    multiselect=True, min_selection_count=0)
 
     for option in options:
@@ -117,9 +135,24 @@ def user_input_side():
 
 
 def user_input_combo():
-    c = Combo()
     # ask user for input and store it in combo object
     # a combo must include one burger, one side, and one drink
+
+    b = user_input_burger()
+
+    # Do the same for drink
+
+    d = user_input_drink()
+
+    # Do the same for side
+
+    s = user_input_side()
+
+    # Construct combo
+
+    c = Combo(b, d, s)
+
+    # Return combo
     return c
 
 
@@ -135,11 +168,10 @@ def take_order():
         option, index = pick(order_options, order_prompt)
 
         match option:
-            case 'burger': o.add_item(user_input_burger())
-            case 'side': o.add_item(user_input_burger())
-            case 'side': o.add_item(user_input_side())
-            case 'drink': o.add_item(user_input_drink())
-            case 'combo': o.add_item(user_input_combo())
+            case 'burger':  o.add_item(user_input_burger())
+            case 'side':    o.add_item(user_input_side())
+            case 'drink':   o.add_item(user_input_drink())
+            case 'combo':   o.add_item(user_input_combo())
 
         loop_exit_title = "Would you like to add more items to your order, remove items, review your order, or finish your order??"
         order_options = ['I would like to add more items.',
@@ -151,16 +183,16 @@ def take_order():
                 pass
 
             case 1:
-                option, index = pick(
-                    o.food_items, "Please select the item to remove.")
                 o.remove_item(option)
 
             case 2:
-                print(o.food_items.__str__)
+                o.review_order()
                 input("Press Enter to continue...")
 
             case 3:
                 break
+
+        # Check automatically if the current list of food items can be put in a combo
 
     # Display a thank you message and order details
 
